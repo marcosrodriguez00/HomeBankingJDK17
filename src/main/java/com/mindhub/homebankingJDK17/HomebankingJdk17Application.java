8b80c5c8718ce5584de1eb3f08ae1353d51ac406
Rodrijojo <marcosrodriguez3000@gmail.com>
2023-12-19 12:59:33 -0300

    first commit

---
+++ src/main/java/com/mindhub/homebankingJDK17/HomebankingJdk17Application.java
@@ -0,0 +1,13 @@
+package com.mindhub.homebankingJDK17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebankingJdk17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebankingJdk17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindhub/homebankingJDK17/HomebankingJdk17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindhub.homebankingJDK17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebankingJdk17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
